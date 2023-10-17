ecaec97f3cfd9a3d1401dae348798483b4df2571
khurram <khurram.akram43@gmail.com>
2023-10-18 02:11:57 +0500

    Print work

--- pos/print.php
+++ pos/print.php
@@ -1,22 +1,94 @@
 <?php
-
 // require '../vendor/autoload.php'; // Include the Composer autoloader
 // require_once '../vendor/tecnickcom/tcpdf/tcpdf.php'; // Include TCPDF library
 
 require 'fpdf/fpdf.php';
 $pdf = new FPDF('P','mm',array(80,200));
 
+// Add a page to the PDF
 $pdf->AddPage();
 
+// Set font and font size for the title (centered)
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(60, 10, 'Brown Munde', 0, 0, 'C');
+$pdf->Cell(60,8,'Brown Munde',1,1,'C'); // 'C' parameter for center alignment
 
-// $imagePath = '../assets/images/brownmude.png'; // Replace with the path to your image file
-// $pdf->Image($imagePath, 20, 20, 30); // Parameters: image path, x, y, width
+// Set font and font size for the order ID (left-aligned)
+$pdf->SetFont('Arial','B',8);
+$pdf->Cell(60,5,'Address:7-9 Princes street, Kilmarnock, KA1 3DD', 0, 1,'C'); // 'L' parameter for left alignment
+$pdf->Cell(60,5,'Phone Number:01563 508050', 0, 1,'C'); // 'L' parameter for left alignment
+$pdf->Cell(60,5,'Email Address:brown_munde@gmail.com', 0, 1,'C'); // 'L' parameter for left alignment
+$pdf->Cell(60,5,'Website:www.brownmunde.co.uk', 0, 1,'C'); // 'L' parameter for left alignment
+
+$pdf->Line(7,38,72,38);
+
+$pdf->Ln(1);
+
+$pdf->SetFont('Arial','BI',8);
+$pdf->Cell(20,10,'Order-Id:123456789',0,0,'C');
+ 
+$pdf->Cell(60,10,'Table-Id:01',0,1,'C'); 
+
+$pdf->Cell(2,5,'Date:',0,0,'C'); // 'C' parameter for center alignment
+$pdf->Cell(20,5,'23-18-13',0,1,'C'); // 'C' parameter for center alignment
+
+// $pdf->Line(7,50,72,50);
+
+//
+$pdf->SetX(7);
+$pdf->SetFont('courier','B',8);
+$pdf->Cell(30,8,"Product",1,0,'C');
+$pdf->Cell(15,8,"Qty",1,0,'C');
+$pdf->Cell(20,8,"Customize",1,1,'C');
+// $pdf->Cell(12,8,"Price",1,1,'C');
+
+for($i=0;$i<6;$i++){
+    $pdf->SetX(7);
+    $pdf->SetFont('courier','B',8);
+    $pdf->Cell(30,8,"Product1",1,0,'C');
+    $pdf->Cell(15,8,$i+1,1,0,'C');
+    $pdf->Cell(20,8,"Customize",1,1,'C');
+}
+
+// 
 
 
+
+
+
+$pdf->SetX(7);
+$pdf->SetFont('courier','B',8);
+$pdf->Cell(20,5,'',0,0,'L'); // 'C' parameter for center alignment
+$pdf->Cell(25,5,'SubTotal',1,0,'C'); // 'C' parameter for center alignment
+$pdf->Cell(20,5,'100',1,1,'C'); // 'C' parameter for center alignment
+
+
+$pdf->SetX(7);
+$pdf->SetFont('courier','B',8);
+$pdf->Cell(20,5,'',0,0,'L'); // 'C' parameter for center alignment
+$pdf->Cell(25,5,'Discount',1,0,'C'); // 'C' parameter for center alignment
+$pdf->Cell(20,5,'0',1,1,'C'); // 'C' parameter for center alignment
+
+$pdf->SetX(7);
+$pdf->SetFont('courier','B',8);
+$pdf->Cell(20,5,'',0,0,'L'); // 'C' parameter for center alignment
+$pdf->Cell(25,5,'Payable',1,0,'C'); // 'C' parameter for center alignment
+$pdf->Cell(20,5,'100',1,1,'C'); // 'C' parameter for center alignment
+
+$pdf->SetX(7);
+$pdf->SetFont('courier','B',8);
+$pdf->Cell(20,5,'',0,0,'L'); // 'C' parameter for center alignment
+$pdf->Cell(25,5,'Payment',1,0,'C'); // 'C' parameter for center alignment
+$pdf->Cell(20,5,'Cash',1,1,'C'); // 'C' parameter for center alignment
+
+
+// $pdf->Cell(20,5,"",0,0);
+// $pdf->SetFont('courier','B',8);
+// $pdf->Cell(20,5,'Important notice',0,0,'L'); // 'C' parameter for center alignment
+
+ 
+
+// Output the PDF
 $pdf->Output();
-
 
 
 
